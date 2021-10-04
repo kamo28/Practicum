2cfa846b9cdd85cc90991fe5b76fd0f1b28bad01
kamo28 <67349755+kamo28@users.noreply.github.com>
2021-10-03 20:38:42 -0500

    Update header.php

--- header.php
+++ header.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="../css/topMenu.css">
   </head>
   <body>
     <header>
