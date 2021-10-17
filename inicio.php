49dfb353a5c56573ffda49b3da8e9485714f8d55
kamo <kamoarito@gmail.com>
2021-10-17 13:04:10 -0500

    Se agrego una landing page

---
+++ inicio.php
@@ -0,0 +1,17 @@
+<?php
+    require "header.php"
+?>
+<head>
+    <title>Inicio - Harinas Elizondo</title>
+</head>
+<style>
+    body {
+        background-image: url('login_resources/universidad.jpg');
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        background-size: cover;
+        -o-background-size: cover;
+    }
+</style>
+</body>
+</html>
